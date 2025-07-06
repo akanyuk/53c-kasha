243f4be3eff3d0de0b798c6edbaae1365845d26d
marinov <marinov@sedmax.ru>
2025-07-06 13:55:39 +0300

    Ambiguous final

--- src/main.asm
+++ src/main.asm
@@ -2,31 +2,33 @@
 	page 0
 	org #6000
 page0s	ei
-	xor a : out (#fe), a 
+	; xor a : out (#fe), a 
 
 	; chunked background
 	ld hl, #4000 
+	push hl
 	ld de, #4001
 	ld (hl), #55
-	ld bc, #0100
+	ld b, e : ld c, l ; ld bc, #0100
 	ldir
 	ld (hl), #AA
 	dec c
 	ldir
-	ld hl, #4000
+	pop hl ; ld hl, #4000
 	ld b, #16
 	ldir
 
 	; bg
 	ld hl, #8000
 	ld bc, 32 * 24 - 1
+	push bc
 	ld a, %00000001
 	call DrawLine
 
 	; botton
 	ld hl, #8000 + 32 * 20
-	ld bc, 32 * 4 - 1
-	ld a, %01000100
+	pop bc ; ld bc, 32 * 4 - 1
+	ld a, %00100000
 	call DrawLine
 
 	; plate
@@ -37,33 +39,95 @@ page0s	ei
 
 	ld hl, #8000 + 32 * 18 + 9
 	ld bc, 13
-	ld a, %00000111
+	; ld a, %00000111  // already here
 	call DrawLine
 
 	ld hl, #8000 + 32 * 17 + 8
 	ld bc, 15
-	ld a, %01000111
+	push bc
+	; ld a, %00000111  // already here
 	call DrawLine
 
 	ld hl, #8000 + 32 * 16 + 8
-	ld bc, 15
-	ld a, %01000111
+	pop bc // ld bc, 15
+	ld a, %00010111
 	call DrawLine
 
-	ld a, 200
-mainLoop	push af
+	call RndLoop
+	ld a, #be : ld (RndLpChk), a
+	call RndLoop
+	
+	ld hl, #8000
+	ld de, #5800
+	ld bc, #300
+	ldir
+
+	ld b, 33
+smokeLoop	push bc
+	ld de, #59ca
+	ld b, 12
+srloop	call RND8
+	cp 59 ; smoke intensivity
+	ld a, %01000110
+	jr c, 1f
+	ld a, %00000001
+1	ld (de), a
+	inc de
+	djnz srloop
+
+	ld b, 4 : call SmokeShift
+	pop bc : djnz smokeLoop
+
+	ld a, 32*13 % 256
+	ld (SmkShftLen + 1), a
+	ld b, 50 : call SmokeShift
+
+	ld b, 104
+	ld hl, #5800 + 32 * 12
+mooh	ld (hl), %00000001
+	inc hl
+	ld (hl), %00000000
+	
+	; mooh snd
+	ld d, 0
+1	ld a, (de)
+	and %00010000
+	out (#fe), a
+2	inc de
+	ld a, d : cp 16
+	jr nz, 1b
+
+	djnz mooh
+
+	jr $
+
+SmokeShift	push bc
+	ld hl, #5860
+	ld de, #5840
+SmkShftLen	ld bc, 32 * 12
+	ldir
+	ld b, 8 : halt : djnz $-1
+	pop bc : djnz SmokeShift
+	ret
+
+RndLoop	
+	ld b, 81
+mainLoop	push bc
 	ld bc, #5800
 	ld hl, #8000	
 
 oneIterr	ld a, (bc)
-	cp (hl)
+RndLpChk	nop // #be cp (hl)
 	jr z, 1f
 	ex de, hl
+
 	call RND8
+	and %00110111
+	
 	ex de, hl
 	ld (bc), a
 
-1	and %00010111
+1	;and %00010111
 	out (#fe), a 
 
 	inc hl
@@ -72,23 +136,17 @@ oneIterr	ld a, (bc)
 
 	halt
 
-	pop af
-	dec a : jr nz, mainLoop
-	
-	out (#fe), a
+	pop bc : djnz mainLoop
+	ret
 
-	ld hl, #8000
-	ld de, #5800
-	ld bc, #300
-	ldir
-
-	jr $
-
-RND8	ld a, 42
+RND8	ld a, 49
 	ld hl, RND_TAB
-	dup 5: add a, (hl): ld (hl), a: inc hl: edup
+	
+	dup 5
+	add a, (hl): ld (hl), a: inc hl
+	edup
+
 	ld (RND8 + 1), a
-	and %01011111
 	ret
 RND_TAB 	
 DrawLine	ld d, h : ld e, l : inc e
@@ -97,4 +155,5 @@ DrawLine	ld d, h : ld e, l : inc e
 	ret
 
 page0e	display /d, 'BIN size: ', $ - page0s
+	display /d, 'Oversize: ', $ - page0s - 256
 	include "src/builder.asm"
