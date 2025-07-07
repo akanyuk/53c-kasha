d27e49ebd13888b4fc9c2a123a20e090344004c7
marinov <marinov@sedmax.ru>
2025-07-07 12:06:26 +0300

    Minification

--- src/main.asm
+++ src/main.asm
@@ -1,17 +1,18 @@
 	device zxspectrum128
 	page 0
-	org #6000
+	org #7800 - 97 ; last param is pause between smoke and mooh
+
 page0s	ei
-	; xor a : out (#fe), a 
+	ld sp, $-2
 
 	; chunked background
 	ld hl, #4000 
 	push hl
 	ld de, #4001
-	ld (hl), #55
+	ld (hl), %01010101
 	ld b, e : ld c, l ; ld bc, #0100
 	ldir
-	ld (hl), #AA
+	ld (hl), %10101010
 	dec c
 	ldir
 	pop hl ; ld hl, #4000
@@ -19,16 +20,17 @@ page0s	ei
 	ldir
 
 	; bg
-	ld hl, #8000
+	ld h, #80 ; ld hl, #8000
+	push hl ; restoring in `showing full image`
 	ld bc, 32 * 24 - 1
 	push bc
 	ld a, %00000001
 	call DrawLine
 
 	; botton
-	ld hl, #8000 + 32 * 20
+	ld hl, #8000 + 32 * 19
 	pop bc ; ld bc, 32 * 4 - 1
-	ld a, %00100000
+	ld a, %00000100
 	call DrawLine
 
 	; plate
@@ -57,17 +59,18 @@ page0s	ei
 	ld a, #be : ld (RndLpChk), a
 	call RndLoop
 	
-	ld hl, #8000
+	; showing full image
+	pop hl ; ld hl, #8000
 	ld de, #5800
-	ld bc, #300
+	ld b, #03 ; ld bc, #300
 	ldir
 
-	ld b, 33
+	ld b, 29 ; smoke duration
 smokeLoop	push bc
-	ld de, #59ca
-	ld b, 12
+	ld de, #59cb
+	ld b, 10
 srloop	call RND8
-	cp 59 ; smoke intensivity
+	cp d ; smoke intensivity
 	ld a, %01000110
 	jr c, 1f
 	ld a, %00000001
@@ -75,64 +78,66 @@ srloop	call RND8
 	inc de
 	djnz srloop
 
-	ld b, 4 : call SmokeShift
+	ld a, 4 : call SmokeShift
 	pop bc : djnz smokeLoop
 
-	ld a, 32*13 % 256
-	ld (SmkShftLen + 1), a
-	ld b, 50 : call SmokeShift
+	; ld a, 32*13 % 256
+	; ld (SmkShftLen + 1), a
+
+	ld hl, SmkShftLen + 1
+	ld (hl), 32*13 % 256
+
+	ld a, l ; ld a, 43 
+	call SmokeShift
 
 	ld b, 104
 	ld hl, #5800 + 32 * 12
 mooh	ld (hl), %00000001
 	inc hl
-	ld (hl), %00000000
+	ld (hl), e ; ld (hl), %00000000
 	
 	; mooh snd
-	ld d, 0
+	ld d, 16
 1	ld a, (de)
 	and %00010000
 	out (#fe), a
-2	inc de
-	ld a, d : cp 16
+	dec de
+	ld a, d : or e
 	jr nz, 1b
 
 	djnz mooh
 
 	jr $
 
-SmokeShift	push bc
-	ld hl, #5860
+SmokeShift	ld hl, #5860
 	ld de, #5840
 SmkShftLen	ld bc, 32 * 12
 	ldir
 	ld b, 8 : halt : djnz $-1
-	pop bc : djnz SmokeShift
+	dec a : jr nz, SmokeShift
 	ret
 
-RndLoop	
-	ld b, 119
+RndLoop	ld b, 101
 mainLoop	push bc
 	ld bc, #5800
 	ld hl, #8000	
 
 oneIterr	ld a, (bc)
-RndLpChk	nop // #be cp (hl)
+RndLpChk	nop // cp (hl) ; #be 
 	jr z, 1f
 	ex de, hl
 
 	call RND8
-	and %00110111
+	and %00010111
 	
 	ex de, hl
 	ld (bc), a
 
-1	;and %00010111
-	out (#fe), a 
+1	out (#fe), a 
 
 	inc hl
 	inc bc
-	ld a, b : cp #5c : jr nz, oneIterr
+	ld a, b : cp #61 : jr nz, oneIterr
 
 	halt
 
@@ -149,7 +154,7 @@ RND8	ld a, 49
 	ld (RND8 + 1), a
 	ret
 RND_TAB 	
-DrawLine	ld d, h : ld e, l : inc e
+DrawLine	ld e, l : ld d, h : inc e
 	ld (hl), a
 	ldir
 	ret
